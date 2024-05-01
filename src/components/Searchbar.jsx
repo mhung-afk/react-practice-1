5faa16eadf13f4ee57ffc7fae3198de4132f8c64
GDuyCats <luongduycat@gmail.com>
2024-05-01 19:31:05 +0700

    search bar fix and data useState

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchIcon from '@mui/icons-material/Search';
-import data from "../MOCK_DATA.json"
+import jsondata from "../MOCK_DATA.json"
 export default function Searchbar() {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        setData(jsondata);
+    }, [])
+    console.log(data)
+
     const [search, setSearch] = useState("")
-    const [editable, setEditable] = useState(true)
-
-    function handlePressEnter(e) {
-        if (e.keyCode === 13) {
-          setEditable(false)
-        }
-      }
-
-    function handleUpdate (e) {
-        setEditable(true)
-        console.log(handleUpdate())
-    }
        
     return (
         <div className="flex flex-col items-center">
@@ -22,30 +17,30 @@ export default function Searchbar() {
                 <SearchIcon className='text-blue-600' />
                 <input className="outline-none px-4 py-2" placeholder='Type to search ...' value={search} onChange={(e) => setSearch(e.target.value)} />
             </div>
-            <div className='text-center mt-16 bg-slate-300 border-black rounded-md'>
+            <div className='text-center absolute top-16 bg-slate-300 border-black rounded-md'>
                 <table className='table-auto'>
                     <thead className='p-32'>
                         <tr>
                             <th className='p-5'>Username</th>
                             <th className='p-5'>Email</th>
                             <th className='p-5'>Birthday</th>
-                            <th className='p-5'><button onClick={(e) => handleUpdate()}className='bg-green-600 border-black rounded-lg w-20'>Edit All</button></th>           
+                            <th className='p-5'><button className='bg-green-600 border-black rounded-lg w-20'>Edit All</button></th>           
                         </tr>
                     </thead>
                     <tbody>
                         {data.filter((item) => {
                             return search.toLowerCase() === '' ? item : 
-                            item.username.toLowerCase().includes(search) 
+                            item.username.toLowerCase().includes(search.toLowerCase()) 
                             ||
-                            item.email.toLowerCase().includes(search)
+                            item.email.toLowerCase().includes(search.toLowerCase())
                             ||
-                            item.birthday.toLowerCase().includes(search)                            
+                            item.birthday.toLowerCase().includes(search.toLowerCase())                            
                         }).map((item, index) => (
                             <tr key={index}>
                                 <td className='p-5'>{item.username}</td>
                                 <td className='p-5'>{item.email}</td>
                                 <td className='p-5'>{item.birthday}</td>
-                                <td><button onClick={(e) => handleUpdate()}className='bg-green-200 border-black rounded-lg w-20'>Edit</button></td>
+                                <td><button className='bg-green-200 border-black rounded-lg w-20'>Edit</button></td>
                             </tr>
                         ))}
                     </tbody>
